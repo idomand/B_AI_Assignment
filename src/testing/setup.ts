b6cf1a1b14dcb543d55d490e98720b28f10e8482
Ido Mandelman <idomand@gmail.com>
2023-09-12 08:59:43 +0200

    basic Accessibility test setup

--- src/testing/setup.ts
+++ src/testing/setup.ts
@@ -1,2 +1,7 @@
-import '@testing-library/jest-dom';
-import '@testing-library/jest-dom/extend-expect';
+import "@testing-library/jest-dom";
+import "@testing-library/jest-dom/extend-expect";
+
+import { expect } from "vitest";
+import matchers from "@testing-library/jest-dom/matchers";
+
+expect.extend(matchers);
